680b50079e57cd95aad526027a361670eaf9e5fe
kwshimhyunbo <adslbna2@naver.com>
2015-08-27 15:36:59 +0900

    위도 경도 추가

--- app/src/main/java/com/example/admin/biojima/Hyunbo.java
+++ app/src/main/java/com/example/admin/biojima/Hyunbo.java
@@ -7,20 +7,23 @@ import java.util.ArrayList;
  */
 public class Hyunbo {
 
-    ArrayList<String> attraction;
+    static String myKey = "Si1LZhStHnfooZIH3OW%2BV5kMa9%2BoJy6u7wuOlqfeIXbSAAcBD%2FXOrOvJsKIRNlprnQVfK8%2B2Je%2BgMUXhcEznwg%3D%3D";
+    static ArrayList<String> attraction;
+    static Double X = 127.0409111; //경도
+    static Double Y = 37.65508056; //위도
 
     Hyunbo()
     {
 
     }
 
-    public void setTouristAttraction(Double x,Double y)
+
+    public void setTouristAttraction(Double x, Double y,int radious)
     {
         //사용자의 현재위치를 Parameters로 50km 근방의 여행지 정보를 할당한다.
 
         attraction = new ArrayList<String>();
 
         //Add to code
-
     }
 }
