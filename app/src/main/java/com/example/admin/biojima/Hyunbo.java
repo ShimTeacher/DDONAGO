f37bae3e4a93a85d8ec543984d96a50e7f6796df
kwshimhyunbo <adslbna2@naver.com>
2015-08-28 02:34:13 +0900

    탭호스트 구현 완

--- app/src/main/java/com/example/admin/biojima/Hyunbo.java
+++ app/src/main/java/com/example/admin/biojima/Hyunbo.java
@@ -1,6 +1,10 @@
 package com.example.admin.biojima;
 
+import android.app.Activity;
 import android.util.Log;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TabHost;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -11,14 +15,18 @@ import java.util.ArrayList;
 /**
  * Created by adslbna2 on 15. 8. 24..
  */
-public class Hyunbo {
+public class Hyunbo extends Activity{
+
+
+
+
 
     //h
     static final String myKey = "Si1LZhStHnfooZIH3OW%2BV5kMa9%2BoJy6u7wuOlqfeIXbSAAcBD%2FXOrOvJsKIRNlprnQVfK8%2B2Je%2BgMUXhcEznwg%3D%3D";
     static ArrayList<String> attraction;
     static Double X = 127.0409111; //경도
     static Double Y = 37.65508056; //위도
-    static String appName= "biozima";
+    static String appName= "nonennal";
 
 
     Hyunbo()
--- app/src/main/java/com/example/admin/biojima/MainActivity.java
+++ app/src/main/java/com/example/admin/biojima/MainActivity.java
@@ -1,30 +1,38 @@
 package com.example.admin.biojima;
 
+import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TabHost;
 
 import org.json.JSONException;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
+
+
+    //TestCode
     static Double X = 127.0409111; //경도
     static Double Y = 37.65508056; //위도
     static int radious = 3;
+    //TestCode
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Hyunbo hb = new Hyunbo();
-        try {
-            hb.setTouristAttraction(X,Y,radious);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
+       tabSetting();
 
     }
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -49,5 +57,27 @@ public class MainActivity extends ActionBarActivity {
 
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void tabSetting() //메뉴 기본탭을 셋팅한다.
+    {
+        TabHost tabhost = (TabHost)findViewById(R.id.tabHost);
+        tabhost.setup();
+
+        TabHost.TabSpec spec1 = tabhost.newTabSpec("Tab1").setContent(R.id.tab1).setIndicator(getString(R.string.tab1));
+        TabHost.TabSpec spec2 = tabhost.newTabSpec("Tab2").setContent(R.id.tab2).setIndicator(getString(R.string.tab2));
+        TabHost.TabSpec spec3 = tabhost.newTabSpec("Tab3").setContent(R.id.tab3).setIndicator(getString(R.string.tab3));
+        //TabHost.TabSpec spec4 = tabhost.newTabSpec("Tab3").setContent(R.id.tab4).setIndicator(getString(R.string.tab4));
+
+        tabhost.addTab(spec1);
+        tabhost.addTab(spec2);
+        tabhost.addTab(spec3);
+        //tabhost.addTab(spec4);
+
+        tabhost.getTabWidget().getChildAt(0).getLayoutParams().height=80;
+        tabhost.getTabWidget().getChildAt(1).getLayoutParams().height=80;
+        tabhost.getTabWidget().getChildAt(2).getLayoutParams().height=80;
+        //tabhost.getTabWidget().getChildAt(3).getLayoutParams().height=80;
+
     }
 }
